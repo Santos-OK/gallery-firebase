12632df6de2ed743841ef187a4c9bfdeb01a5916
Santos-OK <omarsantos@MacBook-Pro-de-Omar-4.local>
2025-09-22 20:45:00 -0600

    chequeo

---
+++ gallery-firebase/src/components/gallery.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ function Gallery() {
+  return (
+    <div>gallery</div>
+  )
+}
+export default Gallery
---
+++ gallery-firebase/src/components/leyenda.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function Leyenda() {
+  return (
+    <footer>
+        <div className='Leyenda'>
+            <p>El pasado es imborrable y el futuro impredecible... Entonces ¯\_(ツ)_/¯</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Leyenda
